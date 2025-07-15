505e1ba8c99eb9f156d419ae8eda8bd8b977ae39
Manoj Kumar Ravi Kumar <manojkumarr0925@gmail.com>
2025-07-15 16:04:22 -0500

    Update main.jsx

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,11 +1,10 @@
-// src/main.jsx
 import React from "react";
 import ReactDOM from "react-dom/client";
-import ChatOnly from "./ChatOnly"; // 👈 replace App with ChatOnly
+import App from "./App";
 import "./index.css";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <ChatOnly />
+    <App />
   </React.StrictMode>
 );
