63491e4177e86d16ba2f7556dab127885997d92b
Manoj Kumar Ravi Kumar <manojkumarr0925@gmail.com>
2025-09-25 18:43:45 -0500

    Update ChatPopup.jsx

--- frontend/src/components/ChatPopup.jsx
+++ frontend/src/components/ChatPopup.jsx
@@ -2,7 +2,7 @@
 import React, { useState, useRef, useEffect } from "react";
 import ChatMessage from "./ChatMessage";
 
-const BASE_URL = "https://omi-chatbot-355024965259.us-central1.run.app";
+const BASE_URL = "https://omi-backend-355024965259.us-central1.run.app";
 
 export default function ChatPopup({ onClose }) {
   const [messages, setMessages] = useState([]);
